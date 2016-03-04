ddc381ba3d6af84afe5cb24e0c17982710620aab
Avi Samloff <avi.samloff@gmail.com>
2016-03-03 16:10:30 -0800

    enables one-time voting via cookies

---
+++ client/misc/cookiesFactory.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module('ballotime')
+    .factory('cookiesF', cookiesF);
+
+  cookiesF.$inject = ['$cookies'];
+
+  function cookiesF($cookies) {
+
+    function addToCookie(cookie, newData) {
+      var current = $cookies.get(cookie);
+      if (!current.length) {
+        $cookies.put(cookie, newData);
+      } else {
+        $cookies.put(cookie, current + ',' + newData);
+      }
+    }
+
+    function getCookie(cookie) {
+      return $cookies.get(cookie);
+    }
+
+    return {
+      addToCookie: addToCookie,
+      getCookie: getCookie
+    };
+
+  }
+
+})();
--- client/vote/controller.js
+++ client/vote/controller.js
@@ -4,15 +4,18 @@
   angular.module('ballotime')
     .controller('voteC', voteC);
 
-  voteC.$inject = ['$scope', 'voteF', '$stateParams'];
+  voteC.$inject = ['$scope', 'voteF', '$stateParams', 'cookiesF'];
 
-  function voteC($scope, voteF, $stateParams) {
+  function voteC($scope, voteF, $stateParams, cookiesF) {
+
     var self = this;
+
     self.ballot = $stateParams.ballot;
+    self.votedBallots = cookiesF.getCookie('votedCookie');
+
     if (self.ballot) {
       voteF.getTotalVotes(self.ballot);
     }
-
 
     self.getBallot = function() {
       voteF.getBallot(self.ballotIdInput)
@@ -35,12 +38,18 @@
     self.submitVote = function($event) {
       var option = $event.currentTarget.attributes.id.value;
       var ballotId = self.ballot.id;
-      voteF.submitVote(option, ballotId)
-        .then(function(data) {
-          self.ballot = data;
-          voteF.getTotalVotes(self.ballot);
-          voteF.setBarType(self.ballot, self);
-        });
+      if (_.includes(self.votedBallots, ballotId)) {
+        console.log('you already voted on this ballot');
+      } else {
+        voteF.submitVote(option, ballotId)
+          .then(function(data) {
+            self.ballot = data;
+            voteF.getTotalVotes(self.ballot);
+            voteF.setBarType(self.ballot, self);
+            cookiesF.addToCookie('votedCookie', ballotId);
+          });
+      }
+
     };
 
   }
