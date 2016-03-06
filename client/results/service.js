fea7c50a4309a8c44a33b1365eca26827f9ef414
Avi Samloff <avi.samloff@gmail.com>
2016-03-06 08:02:09 -0800

    enables read-only results view

--- client/results/service.js
+++ client/results/service.js
@@ -4,10 +4,52 @@
   angular.module('ballotime')
     .factory('resultsF', resultsF);
 
-  resultsF.$inject = [];
+  resultsF.$inject = ['voteF'];
 
-  function resultsF() {
-    return {};
+  function resultsF(voteF) {
+
+    function setBarType(ballot, context) {
+      var one = ballot.option_1_votes;
+      var two = ballot.option_2_votes;
+      var three = ballot.option_3_votes;
+      var four = ballot.option_4_votes;
+      if (Math.max(one, two, three, four) === one) {
+        context.type1 = 'success';
+        context.type2 = '';
+        context.type3 = '';
+        context.type4 = '';
+      } else
+      if (Math.max(one, two, three, four) === two) {
+        context.type1 = '';
+        context.type2 = 'success';
+        context.type3 = '';
+        context.type4 = '';
+      } else
+      if (Math.max(one, two, three, four) === three) {
+        context.type1 = '';
+        context.type2 = '';
+        context.type3 = 'success';
+        context.type4 = '';
+      } else
+      if (Math.max(one, two, three, four) === four) {
+        context.type1 = '';
+        context.type2 = '';
+        context.type3 = '';
+        context.type4 = 'success';
+      }
+    }
+
+    function getTotalVotes(ballot) {
+      ballot.totalVotes = ballot.option_1_votes + ballot.option_2_votes + ballot.option_3_votes + ballot.option_4_votes;
+    }
+
+    return {
+      setBarType: setBarType,
+      getTotalVotes: getTotalVotes,
+      submitVote: voteF.submitVote,
+      getBallot: voteF.getBallot,
+      getBallotByPrettyId: voteF.getBallotByPrettyId
+    };
   }
 
 })();
