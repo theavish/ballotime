0ec4f2d761eb31a0df08bf2d4708d1cc7b1d155d
Avi Samloff <avi.samloff@gmail.com>
2016-03-08 11:58:28 -0800

    removes users from routes and db

--- server/routes.js
+++ server/routes.js
@@ -3,35 +3,13 @@ var userRoutes = require('./routes/users');
 var ballotRoutes = require('./routes/ballots');
 var jsonparser = require('body-parser').json();
 
-
-/***************/
-/* User Routes */
-/***************/
-
-//get all users
-router.get('/users', userRoutes.getAllUsers);
-
-//get a single user
-router.get('/user/:id', userRoutes.getUser);
-
-//add new user
-router.post('/users', jsonparser, userRoutes.saveUser);
-
-//delete a specific user
-router.delete('/user/:id', userRoutes.deleteUser);
-
-
-/*****************/
-/* Ballot Routes */
-/*****************/
-
 //get all ballots
 router.get('/ballots', ballotRoutes.getAllBallots);
 
 //get a single ballot
 router.get('/ballot/:id', ballotRoutes.getBallot);
 
-//get a single ballot
+//get a single ballot by pretty id
 router.get('/ballot/:id/pretty', ballotRoutes.getBallotByPrettyId);
 
 //create a new ballot
